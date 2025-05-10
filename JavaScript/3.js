6b6f6895e8e50626513947aecce26a115f87891f
Darkness1853 <petya.nguen@bk.ru>
2025-05-11 00:07:18 +0300

    lab13

--- JavaScript/3.js
+++ JavaScript/3.js
@@ -6,7 +6,12 @@ const messages = [
     "Встречаемся через час у кинотеатра."
 ];
 
-messages.forEach((message, index) => {
-    const sender = index % 2 === 0 ? "Друг" : "Вы";
-    console.log(`${sender}: ${message}`);
-});
+for (let i = 0; i < messages.length; i++) {
+    let sender;
+    if (i % 2 === 0) {
+        sender = "Друг";
+    } else {
+        sender = "Вы";
+    }
+    console.log(`${sender}: ${messages[i]}`);
+}
